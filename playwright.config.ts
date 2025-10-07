e4348a1c3502e3a2509ecef6da082db851865cbc
Daniel Villanueva <davillan@redhat.com>
2025-10-07 12:05:18 +0200

    test(e2e): added outputDir to playwright.config
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests/playwright/src',
+  outputDir: './tests/playwright/output/',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: 1,
   workers: 1,
   reporter: [
     ['list'],
-    ['junit', { outputFile: './tests/output/junit-results.xml' }],
-    ['json', { outputFile: './tests/output/json-results.json' }],
-    ['html', { open: 'never', outputFolder: './tests/output/html-results/' }],
+    ['junit', { outputFile: './tests/playwright/output/junit-results.xml' }],
+    ['json', { outputFile: './tests/playwright/output/json-results.json' }],
+    ['html', { open: 'never', outputFolder: './tests/playwright/output/html-results/' }],
   ],
   use: {
     trace: 'retain-on-failure',
